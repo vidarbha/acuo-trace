368ebbbbbe584975348471d715ff2cef7a5841de
hicham medkouri <hicham.medkouri@gmail.com>
2016-08-12 08:50:55 +0100

    Added couple of unit tests for cme csv <-> acuo java mapping

---
+++ src/test/java/com/acuo/collateral/trace/services/CMEParsingTest.java
@@ -0,0 +1,65 @@
+package com.acuo.collateral.trace.services;
+
+import com.acuo.collateral.trace.beans.IrSwap;
+import com.acuo.collateral.trace.beans.Request;
+import com.acuo.common.util.ResourceFile;
+import com.tracegroup.transformer.exposedservices.MomException;
+import com.tracegroup.transformer.exposedservices.RuleException;
+import com.tracegroup.transformer.exposedservices.StructureException;
+import com.tracegroup.transformer.exposedservices.UnrecognizedMessageException;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.time.LocalDate;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class CMEParsingTest {
+
+    @Rule
+    public ResourceFile itrs43 = new ResourceFile("/cme/ITRS-43.csv");
+
+    @Rule
+    public ResourceFile itrs100 = new ResourceFile("/cme/ITRS-100.csv");
+
+    Trace service;
+
+    @Before
+    public void setup() throws MomException, StructureException, RuleException, UnrecognizedMessageException {
+        service = new Trace();
+        service.fromCmeFile(null);
+    }
+
+    @Test
+    public void testCmeToAcuoAndBack() throws MomException, StructureException, RuleException, UnrecognizedMessageException, IOException {
+        FromCmeFileOutputWrapper parse = service.fromCmeFile(itrs43.getContent());
+        List<IrSwap> trades = Arrays.stream(parse.getSwap()).map(IrSwap.class::cast).collect(Collectors.toList());
+        assertThat(trades.size()).isEqualTo(43);
+
+        Request request = new Request();
+        request.setValueDate(LocalDate.now());
+        String file = service.toCmeFile(trades.toArray(),request).getOutput();
+        assertThat(file).isEqualTo(itrs43.getContent());
+
+    }
+
+    @Test
+    public void testCmeToAcuo100() throws MomException, StructureException, RuleException, UnrecognizedMessageException, IOException {
+        FromCmeFileOutputWrapper parse = service.fromCmeFile(itrs100.getContent());
+        List<IrSwap> trades = Arrays.stream(parse.getSwap()).map(IrSwap.class::cast).collect(Collectors.toList());
+        assertThat(trades.size()).isEqualTo(100);
+    }
+
+    @Test
+    public void testCmeToAcuo101() throws MomException, StructureException, RuleException, UnrecognizedMessageException, IOException {
+        FromCmeFileOutputWrapper parse = service.fromCmeFile(itrs100.getContent());
+        List<IrSwap> trades = Arrays.stream(parse.getSwap()).map(IrSwap.class::cast).collect(Collectors.toList());
+        assertThat(trades.size()).isEqualTo(100);
+    }
+
+}
