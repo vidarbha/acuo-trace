b3ca35a2b0ebd76863b69eb0f1c01cd32eabf13c
Hicham Medkouri <hicham@anaxo.io>
2017-05-25 10:05:41 +0100

    OW-540 fixed compilation error following output changes

--- src/main/java/com/acuo/collateral/transform/services/PortfolioImportTransformer.java
+++ src/main/java/com/acuo/collateral/transform/services/PortfolioImportTransformer.java
@@ -3,13 +3,13 @@ package com.acuo.collateral.transform.services;
 import com.acuo.collateral.transform.TransformerContext;
 import com.acuo.collateral.transform.trace.transformer_valuations.Mapper;
 import com.acuo.collateral.transform.trace.transformer_valuations.PortfolioImportOutputWrapper;
-import com.google.common.collect.ImmutableList;
 import lombok.extern.slf4j.Slf4j;
-import org.apache.poi.ss.formula.functions.T;
 
-import java.util.Arrays;
 import java.util.List;
 import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+import static java.util.Arrays.stream;
 
 @Slf4j
 public class PortfolioImportTransformer<T> extends BaseTransformer<T> {
@@ -39,15 +39,21 @@ public class PortfolioImportTransformer<T> extends BaseTransformer<T> {
     }
 
     @Override
-    public List<T> deserialise(byte[] input)
-    {
+    public List<T> deserialise(byte[] input) {
         try {
             PortfolioImportOutputWrapper portfolioImportOutputWrapper = getMapper().portfolioImport(input);
-            return Arrays.stream(portfolioImportOutputWrapper.getOutput()).map(value -> (T) value).collect(Collectors.toList());
-        }
-        catch (Exception e)
-        {
-            log.error("Exception in PortfolioImportTransformer" , e);
+            final Object[] irsb = portfolioImportOutputWrapper.getIRSB();
+            final Object[] irsc = portfolioImportOutputWrapper.getIRSC();
+            final Object[] oisc = portfolioImportOutputWrapper.getOISC();
+            final Object[] frac = portfolioImportOutputWrapper.getFRAC();
+            return Stream.concat(
+                    Stream.concat(
+                            Stream.concat(stream(irsb), stream(irsc)), stream(oisc)
+                    ), stream(frac)
+            ).map(value -> (T) value)
+                    .collect(Collectors.toList());
+        } catch (Exception e) {
+            log.error("Exception in PortfolioImportTransformer", e);
             throw new RuntimeException("Exception in PortfolioImportTransformer", e);
         }
     }
