061af8f62ddd88c55957b276a79b66cd341c066d
Hicham Medkouri <hicham@anaxo.io>
2017-06-19 08:31:44 +0100

    OW-146 fixed compilation issue

--- src/main/java/com/acuo/collateral/transform/services/MarkitTransformer.java
+++ src/main/java/com/acuo/collateral/transform/services/MarkitTransformer.java
@@ -2,7 +2,7 @@ package com.acuo.collateral.transform.services;
 
 import com.acuo.collateral.transform.TransformerContext;
 import com.acuo.collateral.transform.trace.transformer_valuations.Mapper;
-import com.acuo.collateral.transform.trace.transformer_valuations.ToMarkitPvRequestOutputWrapper;
+import com.acuo.collateral.transform.trace.transformer_valuations.ToIRSMarkitOutputWrapper;
 import com.google.common.collect.ImmutableList;
 import com.tracegroup.transformer.exposedservices.MomException;
 import com.tracegroup.transformer.exposedservices.RuleException;
@@ -15,7 +15,7 @@ import java.util.List;
 @Slf4j
 public class MarkitTransformer<T> extends BaseTransformer<T> {
 
-    public MarkitTransformer(Mapper mapper) {
+    MarkitTransformer(Mapper mapper) {
         super(mapper);
     }
 
@@ -27,10 +27,10 @@ public class MarkitTransformer<T> extends BaseTransformer<T> {
     @Override
     public String serialise(List<T> value, TransformerContext context) {
         try {
-            ToMarkitPvRequestOutputWrapper outputWrapper = getMapper().toMarkitPvRequest(value.toArray(), context);
+            ToIRSMarkitOutputWrapper outputWrapper = getMapper().toIRSMarkit(value.toArray(), context);
             return outputWrapper.getOutput();
         } catch (MomException | RuleException | UnrecognizedMessageException | StructureException e) {
-            String msg = String.format("error occurred while mapping the data {} to a list of swaps", value);
+            String msg = String.format("error occurred while mapping the data %s to a list of swaps", value);
             log.error(msg, e);
             throw new RuntimeException(msg, e);
         }
