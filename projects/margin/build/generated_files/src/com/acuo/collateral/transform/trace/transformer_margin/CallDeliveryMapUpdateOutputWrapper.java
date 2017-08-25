e829e6e1d9b883099bef507ad74fd303418deccc
Stafani <stafani.wan@acuo.com>
2017-08-25 12:10:34 +0800

    OW-497 Added mapping for fetching agreement details

---
+++ projects/margin/build/generated_files/src/com/acuo/collateral/transform/trace/transformer_margin/CallDeliveryMapUpdateOutputWrapper.java
@@ -0,0 +1,20 @@
+package com.acuo.collateral.transform.trace.transformer_margin;
+
+import com.tracegroup.transformer.exposedservices.*;
+
+/**
+ * This class is auto-generated to define the objects returned by the ManyToMany
+ * ExposedServiceOperation CallDeliveryMapUpdate.
+ */
+public class CallDeliveryMapUpdateOutputWrapper {
+  private String Output;
+
+  public String getOutput() {
+    return Output;
+  }
+
+  public void setOutput(String Output) {
+    this.Output = Output;
+  }
+
+}
