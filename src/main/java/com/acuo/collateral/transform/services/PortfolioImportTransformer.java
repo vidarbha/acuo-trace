7a373a52de821e0b3f135cccea71ad1acf9d0ed6
vpgarg <vgarg.pune@gmail.com>
2017-09-04 22:11:12 +0800

    OW-609 Code changes to persist errors from Valuation Services

--- src/main/java/com/acuo/collateral/transform/services/PortfolioImportTransformer.java
+++ src/main/java/com/acuo/collateral/transform/services/PortfolioImportTransformer.java
@@ -43,15 +43,31 @@ public class PortfolioImportTransformer<T> extends BaseTransformer<T> {
         try {
             PortfolioImportOutputWrapper portfolioImportOutputWrapper = getMapper().portfolioImport(input);
             final Object[] irsb = portfolioImportOutputWrapper.getIRSB();
+            final Object[] irsb_errors = portfolioImportOutputWrapper.getIRSBError();
+            Stream<Object> irsb_stream = Stream.concat(stream(irsb), stream(irsb_errors));
+
             final Object[] irsc = portfolioImportOutputWrapper.getIRSC();
+            final Object[] irsc_errors = portfolioImportOutputWrapper.getIRSCError();
+            Stream<Object> irsc_stream = Stream.concat(stream(irsc), stream(irsb_errors));
+
             final Object[] oisc = portfolioImportOutputWrapper.getOISC();
+            final Object[] oisc_errors = portfolioImportOutputWrapper.getOISCError();
+            Stream<Object> oisc_stream = Stream.concat(stream(oisc), stream(oisc_errors));
+
             final Object[] frac = portfolioImportOutputWrapper.getFRAC();
+            final Object[] frac_errors = portfolioImportOutputWrapper.getFRACError();
+            Stream<Object> frac_stream = Stream.concat(stream(frac), stream(frac_errors));
+
+            final Object[] fxsw = portfolioImportOutputWrapper.getFXSW();
+            final Object[] fxsw_errors = portfolioImportOutputWrapper.getFXSWError();
+            Stream<Object> fxsw_stream = Stream.concat(stream(fxsw), stream(fxsw_errors));
+
             return Stream.concat(
                     Stream.concat(
-                            Stream.concat(stream(irsb), stream(irsc)), stream(oisc)
-                    ), stream(frac)
-            ).map(value -> (T) value)
-                    .collect(Collectors.toList());
+                            Stream.concat(
+                                    Stream.concat(irsb_stream, irsc_stream), oisc_stream
+                            ), frac_stream)
+                    , fxsw_stream).map(value -> (T) value).collect(Collectors.toList());
         } catch (Exception e) {
             log.error("Exception in PortfolioImportTransformer", e);
             throw new RuntimeException("Exception in PortfolioImportTransformer", e);
