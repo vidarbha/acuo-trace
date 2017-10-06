69c041423d74aac75ab9775756d5785bf35605aa
yuansilin <silin@acuo.com>
2017-10-06 16:58:27 +0800

    OW-497, reimport MSError, accept the results

--- src/main/java/com/acuo/collateral/transform/trace/tools/ExternalObjectImport.java
+++ src/main/java/com/acuo/collateral/transform/trace/tools/ExternalObjectImport.java
@@ -1,5 +1,6 @@
 package com.acuo.collateral.transform.trace.tools;
 
+import com.acuo.common.model.results.MSError;
 import com.acuo.common.model.trade.SwapTrade;
 import com.tracegroup.transformer.actionrequests.ReqExternalObjectImport;
 import com.tracegroup.transformer.tools.CmdLineProgressMeter;
@@ -9,11 +10,11 @@ import java.util.List;
 
 public class ExternalObjectImport {
 
-    private static Class[] classes = {SwapTrade.class/*, FixedConvention.class, FixedIborConvention.class, FloatConvention.class*/};
+    private static Class[] classes = {MSError.class/*, FixedConvention.class, FixedIborConvention.class, FloatConvention.class*/};
 
     public static void main(String[] args) {
         ReqExternalObjectImport req = new ReqExternalObjectImport ("External Import");
-        req.setTpjFileName("./projects/valuation/valuation.tpj");
+        req.setTpjFileName("./projects/agreement/Agreement.tpj");
         req.setExternalObjectManagerDefinitionName("AcuoExternalObjectManager");
         List<Class> list = Arrays.asList(classes);
         for (Class aClass : list) {
