ca70394d194ee524c30bffb1f03e498f6f01a845
Hicham Medkouri <hicham@anaxo.io>
2017-10-26 23:33:56 +0100

    OW-869 extracting acuo model to a standalone project

--- src/main/java/com/acuo/collateral/transform/modules/TransformerModule.java
+++ src/main/java/com/acuo/collateral/transform/modules/TransformerModule.java
@@ -10,5 +10,11 @@ public class TransformerModule extends AbstractModule {
     @Override
     protected void configure() {
         bind(MarginCall.class);
+        bind(com.acuo.collateral.transform.trace.transformer_clarus.Service.class);
+        bind(com.acuo.collateral.transform.trace.transformer_markit.Service.class);
+        bind(com.acuo.collateral.transform.trace.transformer_reuters.Service.class);
+        bind(com.acuo.collateral.transform.trace.transformer_agreement.MarginAgreement.class);
+        bind(com.acuo.collateral.transform.trace.transformer_cme.Service.class);
+        bind(com.acuo.collateral.transform.trace.transformer_portfolio.Service.class);
     }
 }
