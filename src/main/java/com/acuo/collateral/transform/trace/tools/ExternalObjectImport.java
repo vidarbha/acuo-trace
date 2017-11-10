5eff29c159ba22b4bc62e8a75f3c689397d89c4b
Hicham Medkouri <hicham@anaxo.io>
2017-11-10 10:36:32 +0000

    OW-828 fixed and imported Agreement into the global definitions

--- src/main/java/com/acuo/collateral/transform/trace/tools/ExternalObjectImport.java
+++ src/main/java/com/acuo/collateral/transform/trace/tools/ExternalObjectImport.java
@@ -1,20 +1,42 @@
 package com.acuo.collateral.transform.trace.tools;
 
-import com.acuo.common.model.results.MSError;
-import com.acuo.common.model.trade.SwapTrade;
+import com.acuo.common.model.agreements.Agreement;
+import com.opengamma.strata.basics.currency.Currency;
+import com.opengamma.strata.basics.date.BusinessDayConvention;
+import com.opengamma.strata.basics.date.DayCount;
+import com.opengamma.strata.basics.date.HolidayCalendarId;
+import com.opengamma.strata.basics.schedule.Frequency;
+import com.opengamma.strata.basics.schedule.RollConvention;
+import com.opengamma.strata.product.common.PayReceive;
+import com.opengamma.strata.product.swap.FixingRelativeTo;
 import com.tracegroup.transformer.actionrequests.ReqExternalObjectImport;
 import com.tracegroup.transformer.tools.CmdLineProgressMeter;
+import lombok.Data;
 
+import java.time.LocalDate;
 import java.util.Arrays;
 import java.util.List;
 
 public class ExternalObjectImport {
 
-    private static Class[] classes = {MSError.class/*, FixedConvention.class, FixedIborConvention.class, FloatConvention.class*/};
+    @Data
+    public static class Test {
+        private BusinessDayConvention businessDayConvention;
+        private Currency currency;
+        private DayCount dayCount;
+        private FixingRelativeTo fixingRelativeTo;
+        private Frequency frequency;
+        private HolidayCalendarId holidayCalendarId;
+        private LocalDate localDate;
+        private PayReceive payReceive;
+        private RollConvention rollConvention;
+    }
+
+    private static Class[] classes = {Agreement.class};
 
     public static void main(String[] args) {
         ReqExternalObjectImport req = new ReqExternalObjectImport ("External Import");
-        req.setTpjFileName("./projects/agreement/Agreement.tpj");
+        req.setTpjFileName("./projects/global/global.tpj");
         req.setExternalObjectManagerDefinitionName("AcuoExternalObjectManager");
         List<Class> list = Arrays.asList(classes);
         for (Class aClass : list) {
