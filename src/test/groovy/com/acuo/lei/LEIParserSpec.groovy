88fecfdc07ef39b845509ee89cbadfb130eb32d8
hicham medkouri <hicham.medkouri@gmail.com>
2018-02-19 16:02:47 +0000

    IL-2 trying Transformer SAT API to handle large LEI xml file

--- src/test/groovy/com/acuo/lei/LEIParserSpec.groovy
+++ src/test/groovy/com/acuo/lei/LEIParserSpec.groovy
@@ -2,6 +2,12 @@ package com.acuo.lei
 
 import com.acuo.common.util.ResourceFile
 import com.acuo.lei.parser_level1.LEIParser
+import com.tracegroup.transformer.configfiles.ConfigFile
+import com.tracegroup.transformer.configfiles.Project
+import com.tracegroup.transformer.dad.ExposedServiceOperation
+import com.tracegroup.transformer.sat.SATFileWriter
+import com.tracegroup.transformer.sat.SAXSATServiceOperator
+import com.tracegroup.transformer.sat.SAXSATSource
 import org.junit.Rule
 import spock.lang.Ignore
 import spock.lang.Specification
@@ -37,4 +43,34 @@ class LEIParserSpec extends Specification {
         csv != null
     }
 
+    void "call transformer Streaming API to deal with large xml file"(){
+        given:
+        Project p = new Project("./projects/lei/lei.tpj")
+        ConfigFile opCf = p.getConfigFile("ExposedServices/LEIParser/xmlToCsv")
+        ExposedServiceOperation inner = (ExposedServiceOperation)p.getRunTimeObject(opCf)
+
+        //Set up the list of nodes that we are interested in - in this case only the Body
+        SAXSATServiceOperator sso = new SAXSATServiceOperator(inner)
+        Map<String, SAXSATServiceOperator> map = new HashMap<String, SAXSATServiceOperator>()
+        map.put("LEIRecord", sso)
+
+        //Create the Source
+        SAXSATSource source  = new SAXSATSource(map)
+
+        //Set up the Result Handler – the output records are separated by a Windows EOL characters
+        File f = File.createTempFile("tmp", "txt")
+        //f.deleteOnExit();
+
+        SATFileWriter writer = new SATFileWriter(f, "\r\n")
+        sso.setResultHandler(writer)
+
+        //Get the source data
+        source.setInputSource(smallXml.createInputStream())
+
+        //Run it
+        source.provide()
+
+        expect:
+        1 == 1
+    }
 }
